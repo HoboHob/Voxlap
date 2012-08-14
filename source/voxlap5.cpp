8c2a2148f21c572dc51abba13f54125a5a373564
John Ericson <Ericson2314@Yahoo.com>
2012-08-13 20:12:41 -0700

    third C alternative for 'updatereflects' now in use struct (.z .y .z) changed to array ([0] [1] [2])

--- source/voxlap5.cpp
+++ source/voxlap5.cpp
@@ -10224,7 +10224,6 @@ static void updatereflects (vx5sprite *spr)
 		}
 
 		fx = 0.0; fy = 0.5; fz = 1.0;
-		#ifdef NOASM
 		tp.x = (sprs.x*fx + sprs.y*fy + sprs.z*fz)*16.0;
 		tp.y = (sprh.x*fx + sprh.y*fy + sprh.z*fz)*16.0;
 		tp.z = (sprf.x*fx + sprf.y*fy + sprf.z*fz)*16.0;
@@ -10233,7 +10232,7 @@ static void updatereflects (vx5sprite *spr)
 			f = tp.x*univec[i].x + tp.y*univec[i].y + tp.z*univec[i].z + 48;
 			for(k=lightcnt-1;k>=0;k--)
 			{
-				h = lightlist[k].x*univec[i].x + lightlist[k].y*univec[i].y + lightlist[k].z*univec[i].z;
+				h = lightlist[k][0]*univec[i].x + lightlist[k][1]*univec[i].y + lightlist[k][2]*univec[i].z;
 				if (*(long *)&h < 0) f -= h;
 			}
 			if (f > 255) f = 255;
@@ -10242,84 +10241,6 @@ static void updatereflects (vx5sprite *spr)
 			((unsigned short *)(&kv6colmul[i]))[1] = j;
 			((unsigned short *)(&kv6colmul[i]))[2] = j;
 		}
-		#else
-		hh *= 16*16.f*8.f/2.f;
-		lightlist[lightcnt][0] = (short)((sprs.x*fx + sprs.y*fy + sprs.z*fz)*hh);
-		lightlist[lightcnt][1] = (short)((sprh.x*fx + sprh.y*fy + sprh.z*fz)*hh);
-		lightlist[lightcnt][2] = (short)((sprf.x*fx + sprf.y*fy + sprf.z*fz)*hh);
-		lightlist[lightcnt][3] = (short)(hh*(48/16.0));
-		#ifdef __GNUC__ //gcc inline asm
-		__asm__ __volatile__
-		(
-			".intel_syntax noprefix\n"
-			"punpcklbw	mm5, vx5.kv6col\n"
-			"pxor	mm6, mm6\n"
-			"mov	edx, lightcnt\n"
-			"shl	edx, 3\n"
-			"mov	ecx, 255*8\n"
-		".Lbeglig:\n"
-			"movq	mm3, iunivec[ecx]\n"   //mm3: 256 u[i].z*256 u[i].y*256 u[i].x*256
-			"mov	eax, edx\n"
-			"movq	mm0, lightlist[edx]\n" //mm0: 48*256,0 tp.z*256 tp.y*256 tp.x*256
-			"pmaddwd	mm0, mm3\n"
-			"pshufw	mm2, mm0, 0x4e\n"
-			"paddd	mm0, mm2\n"
-			"sub	eax, 8\n"
-			"js	short .Lendlig\n"
-		".Lbeglig2:\n"
-			"movq	mm1, lightlist[eax]\n" //mm1: 0 tp.z*256 tp.y*256 tp.x*256
-			"pmaddwd	mm1, mm3\n"
-			"pshufw	mm2, mm1, 0x4e\n"
-			"paddd	mm1, mm2\n"
-			"pminsw	mm1, mm6\n"            //16-bits is ugly, but ok here
-			"psubd	mm0, mm1\n"
-			"sub	eax, 8\n"
-			"jns	short .Lbeglig2\n"       //mm0: 00 II ii ii 00 II ii ii
-		".Lendlig:\n"
-			"pshufw	mm0, mm0, 0x55\n"      //mm0: 00 II 00 II 00 II 00 II
-			"pmulhuw	mm0, mm5\n"
-			"movq	kv6colmul[ecx], mm0\n"
-			"sub	ecx, 8\n"
-			"jnc	short .Lbeglig\n"
-			".att_syntax prefix\n"
-		);
-		#endif
-		#ifdef _MSC_VER //msvc inline asm
-		_asm
-		{
-			punpcklbw	mm5, vx5.kv6col
-			pxor	mm6, mm6
-			mov	edx, lightcnt
-			shl	edx, 3
-			mov	ecx, 255*8
-		beglig:
-			movq	mm3, iunivec[ecx]   //mm3: 256 u[i].z*256 u[i].y*256 u[i].x*256
-			mov	eax, edx
-			movq	mm0, lightlist[edx] //mm0: 48*256,0 tp.z*256 tp.y*256 tp.x*256
-			pmaddwd	mm0, mm3
-			pshufw	mm2, mm0, 0x4e
-			paddd	mm0, mm2
-			sub	eax, 8
-			js	short endlig
-		beglig2:
-			movq	mm1, lightlist[eax] //mm1: 0 tp.z*256 tp.y*256 tp.x*256
-			pmaddwd	mm1, mm3
-			pshufw	mm2, mm1, 0x4e
-			paddd	mm1, mm2
-			pminsw	mm1, mm6         //16-bits is ugly, but ok here
-			psubd	mm0, mm1
-			sub	eax, 8
-			jns	short beglig2        //mm0: 00 II ii ii 00 II ii ii
-		endlig:
-			pshufw	mm0, mm0, 0x55   //mm0: 00 II 00 II 00 II 00 II
-			pmulhuw	mm0, mm5
-			movq	kv6colmul[ecx], mm0
-			sub	ecx, 8
-			jnc	short beglig
-		}
-		#endif
-		//NOTE: emms not necessary!
-		#endif
 	}
 }
 
